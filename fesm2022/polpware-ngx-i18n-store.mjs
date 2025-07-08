a4462e19bc2f20991c6e0abd2b61cc446edaf9b5
Tomcat <polpware@gmail.com>
2025-07-08 07:31:45 -0400

    New publish

--- fesm2022/polpware-ngx-i18n-store.mjs
+++ fesm2022/polpware-ngx-i18n-store.mjs
@@ -80,9 +80,9 @@ class ResourceLoaderService {
             return loadJsonUriP(resolvedOptionsUrl);
         });
     }
+    static { this.ɵfac = function ResourceLoaderService_Factory(t) { return new (t || ResourceLoaderService)(i0.ɵɵinject(i0.NgZone)); }; }
+    static { this.ɵprov = /*@__PURE__*/ i0.ɵɵdefineInjectable({ token: ResourceLoaderService, factory: ResourceLoaderService.ɵfac }); }
 }
-ResourceLoaderService.ɵfac = function ResourceLoaderService_Factory(t) { return new (t || ResourceLoaderService)(i0.ɵɵinject(i0.NgZone)); };
-ResourceLoaderService.ɵprov = /*@__PURE__*/ i0.ɵɵdefineInjectable({ token: ResourceLoaderService, factory: ResourceLoaderService.ɵfac });
 (function () { (typeof ngDevMode === "undefined" || ngDevMode) && i0.ɵsetClassMetadata(ResourceLoaderService, [{
         type: Injectable
     }], function () { return [{ type: i0.NgZone }]; }, null); })();
